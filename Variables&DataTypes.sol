34c17742ec19cc6644b8c2e15d29699446ec91f0
farzeen-ali <farzeenaliarif786@gmail.com>
2025-10-14 17:16:53 +0500

    Variables and Data Types in Solidity

---
+++ Variables&DataTypes.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.0;
+
+contract Variables {
+    string public name = "Farzeen";
+    uint public age = 22;
+    int public temperature = -5;
+    bool public isDeveloper = true;
+    address public walletAddress = 0x1234567890123456789012345678901234567890;
+}
+
+// Naming Convention
+// Pascal (MyName), Camel (myName)
